ae846d6e8efb9da08c8ec280da6d6bd4b05044eb
anuu0916 <youjin1939@gmail.com>
2022-07-12 13:51:16 -0700

    Add csv Files

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import pandas as pd
-import sqlite3
 
-data = {
-    'apples' : [3, 2, 0, 1],
-    'oranges' : [0, 3, 7, 2]
-}
+flights = pd.read_csv('flights.csv')
 
-purchases = pd.DataFrame(data, index=['June', 'Robert', 'Lily', 'David'])
-
-print(purchases.loc['June'])
-con = sqlite3.connect("database.db")
+print(flights.groupby(['year']).mean())
